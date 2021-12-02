fc47ea645a7362cc0f43e38e127d405ffc30a3ed
Dan Appel <dan.appel00@gmail.com>
2021-12-01 17:10:43 -0800

    Day 1 solutions

---
+++ 1/part2.swift
@@ -0,0 +1,45 @@
+let testInput = """
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"""
+
+func numIncreasing(input: [Int]) -> Int {
+    var increasing = 0
+
+    _ = input.reduce(nil) { (prev: Int?, curr: Int) -> Int? in
+        guard let prev = prev else {
+            return curr
+        }
+        if curr > prev {
+            increasing += 1
+        }
+        return curr
+    }
+
+    return increasing
+}
+
+func slidingWindowSums(input: [Int]) -> [Int] {
+    (0..<(input.count - 2))
+        .map { start in
+            input[start..<(start+3)]
+                .reduce(0, +)
+        }
+}
+
+func prepare(input: String) -> [Int] {
+    return input
+        .split(separator: "\n")
+        .map { Int($0)! }
+}
+
+print(numIncreasing(input: prepare(input: testInput)))
+print(numIncreasing(input: slidingWindowSums(input: prepare(input: testInput))))
